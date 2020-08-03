05ed20799ea2469982d020aa8def3eb71b54c0fa
BlueChihuahua <mmunk1994@gmail.com>
2020-08-03 04:35:46 -0600

    Stage 1 = simple estimation

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String text = new Scanner(System.in).nextLine();
-        if (text.length() > 100) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
-        }
+        System.out.println(text.length() > 100 ? "HARD" : "EASY");
     }
 }
